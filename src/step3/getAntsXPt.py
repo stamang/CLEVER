5200e1a068e0e0b61d527c23ed437d62051fb4fa
Suzanne <suzanne.tamang@gmail.com>
2016-06-21 13:22:09 -0700

    updated the ptsequencing step for general use

--- src/step3/getAntsXPt.py
+++ src/step3/getAntsXPt.py
@@ -1,19 +1,15 @@
 import os, sys, operator
 from helperFcns import *
 
-version = "V7"
-path = "/data3/mbc"                                                          #mbc path
-oncoKey = path+"/oncoshare/oncoshare_s6_map.txt"                             #oncoshare-S6 map
-noteMdata = path+"/notemetadata.txt"
-#dictfile = "/data3/mbc/src/clever/mbc-dic.txt"
-dictfile = "/data3/clever/mbc/mbc-dic_"+version+".txt"
-
-#first neg, all else pos
-testpts = ["1429499","1158149","1320506","298541","1429499","1619903","1838136","1523543"]
-
-# main #
-target_class = ["mbc","drecur","lrecur","mbcrx","recur","mets"]
-s6pts = getS6ids(0,oncoKey)                                             #S6 ids for subset
+proj = "pqrs"
+ppath = "../../proj/"+proj+"/"
+ptKey = "../../proj/"+proj+"/ptkey.txt"  
+noteMdata = "../../res/corpus/testnotemdata.txt"
+dpath = "../../res/dicts/"
+dictfile = dpath+"clever_terminology.txt"
+# target classes #
+target_class = ["mbc","drecur","lrecur","advp","recur","mets"]
+s6pts = getPids(0,ptKey)                                             #S6 ids for subset
 termDict = getTerminology(dictfile)                                          #get terminology
 
 #for pt in s6pts: print pt,s6pts[pt]
@@ -21,7 +17,8 @@ noteMDict = loadOncoNoteMdata(noteMdata)                                     #pa
 
 seqDict = {}
 for target in target_class:
-    seqFile = "/data3/clever/mbc/seq/"+version+"/"+target+"/extraction*.tsv"
+    seqFile = ppath +"seq/"+target+"/extraction*.tsv"
+    print seqFile
     tmpDict = loadSeqs(seqFile,noteMDict,termDict)
     seqDict.update(tmpDict)
 print len(seqDict)
@@ -29,13 +26,13 @@ print len(seqDict)
 for oncoid in s6pts:
     pt = str(s6pts[oncoid])
 #for pt in testpts:
-#    print pt
+    print pt
     ptAnts = []
-    fout = open(path+"/oncoshare/clever/ptseq/"+version+"/seqs_"+pt+".txt","w") 
+    fout = open(ppath+"/ptseq/seqs_"+pt+".txt","w") 
     for sid in seqDict:
         tmp = seqDict[sid]
         sinfo = tmp.split("|")
-        #print sinfo
+        print sinfo
         if sinfo[4]==pt:
 #            print "found"
             toff = sinfo[7]
